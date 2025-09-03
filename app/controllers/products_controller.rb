d55e1976a8c39fc9a5911aac619a07cf98a96948
sihle.ntuli <sihle.ntuli@Vitality.co.uk>
2025-09-03 11:02:30 +0200

    Resolve intermittent image loading issue on product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,4 @@
+# app/controllers/products_controller.rb
 class ProductsController < ApplicationController
   include Pagy::Backend
 
@@ -15,8 +16,14 @@ class ProductsController < ApplicationController
       base_scope = Product.active
     end
 
-    @q = base_scope.ransack(params[:q])
-    @products = @q.result(distinct: true).includes(:category)
+    # Preload Active Storage images to prevent them from disappearing
+    @q = base_scope.with_attached_images.ransack(params[:q])
+
+    # NOTE: The hard-coded `.order(created_at: :desc)` will prevent Ransack sorting from working.
+    # It's better to let Ransack handle sorting based on the URL parameter.
+    # The correct line to use with Ransack would be:
+    # @products = @q.result(distinct: true).includes(:category)
+    @products = @q.result(distinct: true).includes(:category).order(created_at: :desc)
   end
 
   def show
